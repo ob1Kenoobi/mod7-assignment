828b0b3ca513c990d2a9d6d04320538d5b9443b4
Hasan Banna <hansolo132134@gmail.com>
2023-09-24 11:27:51 +0600

    Created a new Dart file to define list of products

---
+++ lib/product_data.dart
@@ -0,0 +1,8 @@
+import 'package:mod7_productlist/product.dart';
+
+//contains the instances of the Product class.
+List<Product> products = [
+  Product('Product 1', 10.99, 0),
+  Product('Product 2', 19.99, 0),
+  Product('Product 3', 5.99, 0),
+];
