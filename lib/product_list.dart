9874d54cf9626e38fff8fd57fb46a31dd7e98bc2
Hasan Banna <hansolo132134@gmail.com>
2023-09-24 12:00:39 +0600

    added listviewbuilder and ListTile to display the product

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -14,6 +14,38 @@ class ProductList extends StatefulWidget {
 class _ProductListState extends State<ProductList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: widget.products.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ProductCard(product: widget.products[index]);
+      },
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+
+  ProductCard({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        title: Text(product.name),
+        subtitle: Text('Price: \$${product.price.toStringAsFixed(2)}'),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Quantity: ${product.quantity}'),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
